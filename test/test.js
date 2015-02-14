e5754ba301b1183bd6513acfa37da4858df4370b
sasaplus1 <sasaplus1@gmail.com>
2015-02-14 23:24:48 +0900

    updated tests

--- test/test.js
+++ test/test.js
@@ -1,168 +1,208 @@
-describe('cookie', function() {
+(function() {
 
-  function MockCookie() {
-    this.value_ = '';
-    Object.defineProperty(this, 'cookie', {
-      get: function() {
-        return this.value_;
-      },
-      set: function(value) {
-        this.value_ += (this.value_ && ';') + value;
-      }
-    });
-  }
+  'use strict';
 
-  describe('#get()', function() {
+  var assert = this.assert;
 
-    beforeEach(function() {
-      window.mock = new MockCookie;
-      window.mock.cookie = 'name1=value1';
-      window.mock.cookie = 'name2=value2';
-      window.mock.cookie = 'name3=value3';
-      window.mock.cookie = 'iroha=%E3%81%84%E3%82%8D%E3%81%AF';
-      window.mock.cookie = '  space=%20%20%E7%A9%BA%E7%99%BD';
-    });
+  describe('cookie', function() {
 
-    afterEach(function() {
-      delete window.mock;
-    });
+    function MockCookie() {
+      this.value_ = '';
 
-    it('should get any values if not set name', function() {
-      expect(cookie.get()).to.eql({
-        name1: 'value1',
-        name2: 'value2',
-        name3: 'value3',
-        iroha: 'いろは',
-        space: '  空白',
+      Object.defineProperty(this, 'cookie', {
+        get: function() {
+          return this.value_;
+        },
+        set: function(value) {
+          this.value_ += (this.value_ && '; ') + value;
+        }
       });
+    }
+
+    describe('.get()', function() {
+
+      beforeEach(function() {
+        cookie.config.raw = false;
+
+        window.mock = new MockCookie;
+        window.mock.cookie = 'name=value';
+        window.mock.cookie = 'quote=%22quote%22';
+        window.mock.cookie = 'iroha=%E3%81%84%E3%82%8D%E3%81%AF';
+      });
+
+      afterEach(function() {
+        delete window.mock;
+      });
+
+      it('should got cookie-pairs', function() {
+        assert.deepEqual(cookie.get(), {
+          name: 'value',
+          quote: '"quote"',
+          iroha: 'いろは'
+        });
+      });
+
+      it('should got cookie-pairs, raw mode is true', function() {
+        cookie.config.raw = true;
+
+        assert.deepEqual(cookie.get(), {
+          name: 'value',
+          quote: '%22quote%22',
+          iroha: '%E3%81%84%E3%82%8D%E3%81%AF'
+        });
+      });
+
+      it('should got cookie-value', function() {
+        assert(cookie.get('name') === 'value');
+        assert(cookie.get('quote') === '"quote"');
+        assert(cookie.get('iroha') === 'いろは');
+      });
+
+      it('should got cookie-value, raw mode is true', function() {
+        cookie.config.raw = true;
+
+        assert(cookie.get('name') === 'value');
+        assert(cookie.get('quote') === '%22quote%22');
+        assert(cookie.get('iroha') === '%E3%81%84%E3%82%8D%E3%81%AF');
+      });
+
+      it('should got null when cookie-name is not found', function() {
+        assert(cookie.get('unknown') === null);
+      });
+
     });
 
-    it('should get value with name', function() {
-      expect(cookie.get('name1')).to.be('value1');
-      expect(cookie.get('name2')).to.be('value2');
-      expect(cookie.get('name3')).to.be('value3');
-      expect(cookie.get('iroha')).to.be('いろは');
-      expect(cookie.get('space')).to.be('  空白');
-    });
+    describe('.set()', function() {
 
-    it('should return null if not found name', function() {
-      expect(cookie.get('special-name')).to.be(null);
+      beforeEach(function() {
+        cookie.config.raw = false;
+
+        window.mock = new MockCookie;
+      });
+
+      afterEach(function() {
+        delete window.mock;
+      });
+
+      it('should set cookie-pairs', function() {
+        cookie.set([
+          {
+            name: 'name',
+            value: 'value',
+            domain: 'localhost',
+            maxage: 1423839600000
+          },
+          {
+            name: 'quote',
+            value: '"quote"',
+            path: '/',
+            secure: true
+          },
+          {
+            name: 'iroha',
+            value: 'いろは',
+            expires: 'Sat Feb 14 2015 00:00:00 GMT+0900 (JST)'
+          }
+        ]);
+
+        assert(window.mock.cookie ===
+            'name=value; Max-Age=1423839600000; Domain=localhost; ' +
+            'quote=%22quote%22; Path=/; Secure; ' +
+            'iroha=%E3%81%84%E3%82%8D%E3%81%AF; ' +
+            'Expires=Sat Feb 14 2015 00:00:00 GMT+0900 (JST)'
+        );
+      });
+
+      it('should set cookie-pairs, raw mode is true', function() {
+        cookie.config.raw = true;
+
+        cookie.set([
+          {
+            name: 'name',
+            value: 'value',
+            domain: 'localhost',
+            maxage: 1423839600000
+          },
+          {
+            name: 'quote',
+            value: '"quote"',
+            path: '/',
+            secure: true
+          },
+          {
+            name: 'iroha',
+            value: 'いろは',
+            expires: 'Sat Feb 14 2015 00:00:00 GMT+0900 (JST)'
+          }
+        ]);
+
+        assert(window.mock.cookie ===
+            'name=value; Max-Age=1423839600000; Domain=localhost; ' +
+            'quote="quote"; Path=/; Secure; ' +
+            'iroha=いろは; ' +
+            'Expires=Sat Feb 14 2015 00:00:00 GMT+0900 (JST)'
+        );
+      });
+
+      it('should set cookie-pair', function() {
+        cookie.set('name', 'value', {
+          maxage: '1423839600000',
+          domain: 'localhost'
+        });
+        cookie.set('quote', '"quote"', {
+          path: '/',
+          secure: true
+        });
+        cookie.set('iroha', 'いろは', {
+          expires: 'Sat Feb 14 2015 00:00:00 GMT+0900 (JST)'
+        });
+
+        assert(window.mock.cookie ===
+            'name=value; Max-Age=1423839600000; Domain=localhost; ' +
+            'quote=%22quote%22; Path=/; Secure; ' +
+            'iroha=%E3%81%84%E3%82%8D%E3%81%AF; ' +
+            'Expires=Sat Feb 14 2015 00:00:00 GMT+0900 (JST)'
+        );
+      });
+
+      it('should set cookie-pair, raw mode is true', function() {
+        cookie.config.raw = true;
+
+        cookie.set('name', 'value', {
+          maxage: '1423839600000',
+          domain: 'localhost'
+        });
+        cookie.set('quote', '"quote"', {
+          path: '/',
+          secure: true
+        });
+        cookie.set('iroha', 'いろは', {
+          expires: 'Sat Feb 14 2015 00:00:00 GMT+0900 (JST)'
+        });
+
+        assert(window.mock.cookie ===
+            'name=value; Max-Age=1423839600000; Domain=localhost; ' +
+            'quote="quote"; Path=/; Secure; ' +
+            'iroha=いろは; ' +
+            'Expires=Sat Feb 14 2015 00:00:00 GMT+0900 (JST)'
+        );
+      });
+
+      it('can be method chain', function() {
+        cookie
+          .set('name1', 'value1')
+          .set('name2', 'value2')
+          .set('name3', 'value3');
+
+        assert(window.mock.cookie ===
+            'name1=value1; ' +
+            'name2=value2; ' +
+            'name3=value3'
+        );
+      });
+
     });
 
   });
 
-  describe('#get() with unexpected characters', function() {
-
-    var ucNames = [
-          '\x00', '\x01', '\x02', '\x03', '\x04', '\x05', '\x06', '\x07',
-          '\x08', '\x09', '\x0a', '\x0b', '\x0c', '\x0d', '\x0e', '\x0f',
-          '\x10', '\x11', '\x12', '\x13', '\x14', '\x15', '\x16', '\x17',
-          '\x18', '\x19', '\x1a', '\x1b', '\x1c', '\x1d', '\x1e', '\x1f',
-          '\x20', '\x22', '\x28', '\x29', '\x2c', '\x2f', '\x3a', '\x3b',
-          '\x3c', '\x3d', '\x3e', '\x3f', '\x40', '\x5b', '\x5c', '\x5d',
-          '\x7b', '\x7d', '\x7f'
-        ],
-        ucValues = [
-          '\x00', '\x01', '\x02', '\x03', '\x04', '\x05', '\x06', '\x07',
-          '\x08', '\x09', '\x0a', '\x0b', '\x0c', '\x0d', '\x0e', '\x0f',
-          '\x10', '\x11', '\x12', '\x13', '\x14', '\x15', '\x16', '\x17',
-          '\x18', '\x19', '\x1a', '\x1b', '\x1c', '\x1d', '\x1e', '\x1f',
-          '\x20', '\x22', '\x2c', '\x3b', '\x5c', '\x7f'
-        ];
-
-    beforeEach(function() {
-      var i, len;
-
-      window.mock = new MockCookie;
-
-      for (i = 0, len = ucNames.length; i < len; ++i) {
-        window.mock.cookie = ucNames[i] + '=';
-      }
-
-      for (i = 0, len = ucValues.length; i < len; ++i) {
-        window.mock.cookie = ucValues[i].charCodeAt(0) + '=' + ucValues[i];
-      }
-    });
-
-    afterEach(function() {
-      delete window.mock;
-    });
-
-    it('should not get value if name has unexpected character', function() {
-      var i, len;
-
-      for (i = 0, len = ucNames.length; i < len; ++i) {
-        expect(cookie.get(ucNames[i])).to.be(null);
-      }
-    });
-
-    it('should not get value if value has unexpected character', function() {
-      var i, len;
-
-      for (i = 0, len = ucValues.length; i < len; ++i) {
-        expect(cookie.get(ucValues[i].charCodeAt(0))).to.be(null);
-      }
-    });
-
-  });
-
-  describe('#set()', function() {
-
-    beforeEach(function() {
-      window.mock = new MockCookie;
-    });
-
-    afterEach(function() {
-      delete window.mock;
-    });
-
-    it('should set some values if parameter is array', function() {
-      cookie.set([
-        {name: 'name1', value: 'value1', expires: 'Wed, 25 Dec 2013 15:24:47 GMT'},
-        {name: 'name2', value: 'value2', maxage: 60},
-        {name: 'name3', value: 'value3', domain: 'localhost'},
-        {name: 'name4', value: 'value4', path: '/'},
-        {name: 'iroha', value: 'いろは', secure: true}
-      ]);
-
-      expect(window.mock.cookie).to.be(
-        'name1=value1;Expires=Wed, 25 Dec 2013 15:24:47 GMT;' +
-        'name2=value2;Max-Age=60;' +
-        'name3=value3;Domain=localhost;' +
-        'name4=value4;Path=/;' +
-        'iroha=%E3%81%84%E3%82%8D%E3%81%AF;Secure'
-      );
-    });
-
-    it('should set value for name', function() {
-      cookie.set('name1', 'value1', {expires: 'Wed, 25 Dec 2013 15:24:47 GMT'});
-      cookie.set('name2', 'value2', {maxage: 60});
-      cookie.set('name3', 'value3', {domain: 'localhost'});
-      cookie.set('name4', 'value4', {path: '/'});
-      cookie.set('iroha', 'いろは', {secure: true});
-
-      expect(window.mock.cookie).to.be(
-        'name1=value1;Expires=Wed, 25 Dec 2013 15:24:47 GMT;' +
-        'name2=value2;Max-Age=60;' +
-        'name3=value3;Domain=localhost;' +
-        'name4=value4;Path=/;' +
-        'iroha=%E3%81%84%E3%82%8D%E3%81%AF;Secure'
-      );
-    });
-
-    it('can be method chain', function() {
-      cookie
-        .set('name1', 'value1')
-        .set('name2', 'value2')
-        .set('name3', 'value3');
-
-      expect(window.mock.cookie).to.be(
-        'name1=value1;' +
-        'name2=value2;' +
-        'name3=value3'
-      );
-    });
-
-  });
-
-});
+}).call(this);
